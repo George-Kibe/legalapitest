a1bbbc1cb076cb82f86792700be4dfb557fafadd
George Kibe <georgekibewambui@gmail.com>
2022-10-26 13:29:06 +0300

    redirect tests

--- src/Login.js
+++ src/Login.js
@@ -1,49 +1,60 @@
 import React, {useState, useEffect} from 'react'
+import jwt_decode from "jwt-decode";
+import {GrCheckboxSelected, GrCheckbox} from "react-icons/gr"
 
 function Login() {
-  const [checked, setChecked] = useState(true);
+  const [checked, setChecked] = useState(false);
+  const currentURL = window.location.href
+  const strings = currentURL.split("?token=")
+  const token = strings[1] 
+    
   const handleSubmit = () => {
 
   }
   const handleChange = () => {
 
   }
+  useEffect(() => {
+    if(!token){
+        return
+    }
+    const decoded = jwt_decode(token);
+    console.log(decoded.isSuccess)
+    setChecked(decoded.isSuccess)
+    console.log(checked)
+  }, [token])
+  
   return (
     <div style={{padding:30}}>
-        <h1>Sign in to continue</h1>
-        <form onSubmit={handleSubmit} noValidate>
-            <div style={{margin:10}}>
-                <label htmlFor="email">Email: </label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    onChange={handleChange}
-                />
-            </div>
-            <div style={{margin:10}}>
-                <label htmlFor="password">Password: </label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    onChange={handleChange}
-                />
-            </div>
-            <div style={{margin:10}}>
-                <label>
-                    <input type="checkbox"
-                        defaultChecked={checked}
-                        onChange={() => setChecked(!checked)}
-                    />
-                    I agree with to the 
-                    <a href='https://100087.pythonanywhere.com/policy/FB1010000000001665306290565391/website-privacy-policy/?redirect_url=https://george-kibe.github.io/legalapitest/'>
-                         privacy policy and terms and conditions
-                    </a>
-                </label>
-            </div>
-            <button style={{margin:10}} type="submit">Sign In</button>
-        </form>
+        <h1>Sample website content</h1>        
+        {/* <div style={{margin:10}}>
+            <label htmlFor="email">Email: </label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+            />
+        </div>
+        <div style={{margin:10}}>
+            <label htmlFor="password">Password: </label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleChange}
+            />
+        </div> */}
+        <div style={{margin:10}}>
+            {
+                checked? <GrCheckboxSelected /> :<GrCheckbox />
+            }                     
+            <a style={{margin:10}} href='https://100087.pythonanywhere.com/policy/FB1010000000001665306290565391/website-privacy-policy/?redirect_url=https://george-kibe.github.io/legalapitest/'>
+                I agree with to the privacy policy and terms and conditions
+            </a>
+        </div>
+        <button style={{margin:10}} type="submit">Website content continued</button>
+        
     </div>
   )
 }
